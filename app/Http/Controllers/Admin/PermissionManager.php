8bc0834a0c850dd2a0e5cd63d5911d196c29cf60
HongHa0909 <lythihongha090998@gmail.com>
2019-08-30 18:31:11 +0700

    FullVersion

--- app/Http/Controllers/Admin/PermissionManager.php
+++ app/Http/Controllers/Admin/PermissionManager.php
@@ -29,8 +29,8 @@ class PermissionManager extends Controller
         //Get role by role id
         $data['role'] = $this->getrole();
         //get account id then query data of the contract existing and contract should join with type contract
-        $contract = contract::find(Auth::user()->id);
-        $data['contract_user'] = $contract->getContr($contract->id);
+        // $contract = contract::find(Auth::user()->id);
+        $data['contract_user'] = DB::table('account')->select('contract.id','type_contract.name_type','contract.date_end','contract.date_start','contract.name_contract')->join('contract','contract.id_account','account.id')->join('type_contract','type_contract.id','contract.id_type_contract')->where('account.id',Auth::user()->id)->get();
         //query data get info contract
         //after query detail contract -> attendance -> 1 is permission if have
         $data['num'] = 1;
